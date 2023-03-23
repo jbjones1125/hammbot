15b6932c7c1810ff4d6308be19d8db325a4fca2e
jbjones1125 <jacobjones5833@gmail.com>
2023-03-23 13:39:29 -0400

    adjusted responses to include tweet URL and added possible future support for patchnote tweet maintenance announcements

--- bot/al_maintenance.py
+++ bot/al_maintenance.py
@@ -10,19 +10,23 @@ def getTweets(user, amount):
     for i, t in enumerate(scrape.TwitterSearchScraper('from:' + user).get_items()):
         if i > amount:
             break
-        tweets.append([t.date, t.sourceLabel, t.rawContent, t.url])
+        if t.media:
+            tweets.append([t.date, t.sourceLabel, t.rawContent, t.url, t.media])
+        else:
+            tweets.append([t.date, t.sourceLabel, t.rawContent, t.url, 'None'])
     return tweets
 
 
 def getStatus(tweets):
     start = "We will start maintenance on"
+    start2 = "Part 1"
     during = "Here's a 1-hour advance notification for the coming maintenance period"
     extend = "will need to extend the maintenance period"
     end = "Maintenance has ended"
 
     for tweet in tweets:
         if end in tweet[2]:
-            return "No maintenance"
+            return ["No maintenance", tweet[3]]
 
         elif extend in tweet[2]:
             '''
@@ -39,7 +43,7 @@ def getStatus(tweets):
 
         elif during in tweet[2] and (datetime.now(timezone.utc).astimezone() - timedelta(hours=1)) >= tweet[0]:
             '''
-            maintenance length: get number of hours maintenance is said to be, add 1 hour to it 
+            maintenance length: get number of hours maintenance is said to be, add 1 hour to it
                                 because the tweet is posted 1 hour before maintenance begins
             maintenance end time: tweet post time + maintenance length
             time till: subtract time.now() from the calculated maintenance end time
@@ -68,5 +72,14 @@ def getStatus(tweets):
             minutes = int(seconds % 3600 / 60)
             return [f"Maintenance starts in {hours:02d}:{minutes:02d}", tweet[3]]
 
-        else:
-            return ["No maintenance", "https://twitter.com/AzurLane_EN"]
+        # elif start2 in tweet[2]:
+        #     url = tweet[4][1].fullUrl
+        #     request_site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+        #     img_from_url = urllib.request.urlopen(request_site)
+        #     img = Image.open(img_from_url)
+        #     result = pytesseract.image_to_string(img, config='-l eng')
+        #
+        #     return ['Media:', tweet[3]]
+
+        # else:
+        #     return ["No maintenance", "https://twitter.com/AzurLane_EN"]
